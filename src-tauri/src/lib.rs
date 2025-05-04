5b6cbba45e48095ca60ae60998e639e55562fe61
9c23a5 <40715125+9c23a5@users.noreply.github.com>
2025-05-04 15:08:37 +0200

    use own form of prevent-default, block navigation shortcuts on non-dev

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,7 +14,11 @@ fn prevent_default() -> tauri::plugin::TauriPlugin<tauri::Wry> {
     use tauri_plugin_prevent_default::Flags;
 
     tauri_plugin_prevent_default::Builder::new()
-        .with_flags(Flags::all().difference(Flags::DEV_TOOLS | Flags::RELOAD | Flags::CONTEXT_MENU))
+        .with_flags(
+            Flags::all().difference(
+                Flags::DEV_TOOLS | Flags::RELOAD | Flags::CONTEXT_MENU | Flags::NAVIGATION,
+            ),
+        )
         .build()
 }
 
